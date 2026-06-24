8ce9fb15ec579fb86d160ecddc71c7994652fa49
Ahtisham01 <219668890+Ahtisham-1@users.noreply.github.com>
2026-06-24 20:40:44 +0530

    feat: complete sidebar rendering, note selection, and active note highlights

--- script.js
+++ script.js
@@ -15,14 +15,67 @@ let renderNotes = JSON.parse(localStorage.getItem("markdownNotes")) || [];
 function savedNotes() {
   localStorage.setItem("markdownNotes", JSON.stringify(renderNotes));
 }
+
 addNoteButton.addEventListener("click", function () {
+  //Better time id with actuall time and date
+  const options = {
+    day: "numeric",
+    month: "short",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+
+  // Get the string and force lowercase am/pm
+  const uniqueTimeId = new Date()
+    .toLocaleString("en-US", options)
+    .toLowerCase();
+
+  // Notes object
   const newNoteObject = {
-    id: "N" + Date.now(),
+    id: "N" + uniqueTimeId,
     title: "Untitled",
     content: "Type your text here...",
-    updatedAt: Date.now(),
+    updatedAt: uniqueTimeId,
   };
+
   renderNotes.push(newNoteObject);
   savedNotes();
+  NotesTitleFunction();
 });
-console.log(renderNotes);
+
+function NotesTitleFunction() {
+  // clearning the sidebar to avoid duplicates
+  showNotesList.textContent = "";
+
+  renderNotes.forEach((elementTitle) => {
+    // parent div for the below two elements
+    const listContainer = document.createElement("div");
+    if (elementTitle.id === activeNoteId) {
+      listContainer.classList.add("highlight-active-container");
+    } else {
+      listContainer.classList.remove("highlight-active-container");
+    }
+    // notes title
+    const listTitle = document.createElement("h2");
+    listTitle.innerHTML = elementTitle.title;
+    listContainer.appendChild(listTitle);
+
+    // notes timeid
+    const listUpdate = document.createElement("span");
+    listUpdate.innerHTML = elementTitle.updatedAt;
+    listContainer.appendChild(listUpdate);
+
+    // appending the list container with the html main div
+    showNotesList.appendChild(listContainer);
+
+    listContainer.addEventListener("click", function (e) {
+      activeNoteId = elementTitle.id;
+      WritingTextArea.value = elementTitle.content;
+      NotesTitleFunction();
+    });
+  });
+}
+
+NotesTitleFunction();
